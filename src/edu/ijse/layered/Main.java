1ca0c79a0804227357ac7b5c28c294172cba6d67
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-06-23 12:40:27 +0530

    Item Save in view and controller layer

--- src/edu/ijse/layered/Main.java
+++ src/edu/ijse/layered/Main.java
@@ -4,6 +4,8 @@
  */
 package edu.ijse.layered;
 
+import edu.ijse.layered.view.ItemView;
+
 /**
  *
  * @author anjan
@@ -13,8 +15,8 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+        new ItemView().setVisible(true);
     }
     
 }
---
+++ src/edu/ijse/layered/controller/ItemController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.layered.controller;
+
+import edu.ijse.layered.dto.ItemDto;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anjan
+ */
+public class ItemController {
+    
+    public String save(ItemDto itemDto) throws Exception{
+        return null;
+    }
+    
+    public String update(ItemDto itemDto) throws Exception{
+        return null;
+    }
+    
+    public String delete(String code) throws Exception{
+        return null;
+    }
+    
+    public ItemDto get(String code) throws Exception{
+        return null;
+    }
+    
+    public ArrayList<ItemDto> getAll() throws Exception{
+        return null;
+    }
+}
