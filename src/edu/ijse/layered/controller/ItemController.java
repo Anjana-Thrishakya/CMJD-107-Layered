ff301bd2e4f2a78b1944dade2824a7a998f849f2
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-06-23 13:12:33 +0530

    Service with Impl

--- src/edu/ijse/layered/controller/ItemController.java
+++ src/edu/ijse/layered/controller/ItemController.java
@@ -5,6 +5,8 @@
 package edu.ijse.layered.controller;
 
 import edu.ijse.layered.dto.ItemDto;
+import edu.ijse.layered.service.ServiceFactory;
+import edu.ijse.layered.service.custom.ItemService;
 import java.util.ArrayList;
 
 /**
@@ -13,23 +15,25 @@ import java.util.ArrayList;
  */
 public class ItemController {
     
+    private ItemService itemService = (ItemService) ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.ITEM);
+    
     public String save(ItemDto itemDto) throws Exception{
-        return null;
+        return itemService.save(itemDto);
     }
     
     public String update(ItemDto itemDto) throws Exception{
-        return null;
+        return itemService.update(itemDto);
     }
     
     public String delete(String code) throws Exception{
-        return null;
+        return itemService.delete(code);
     }
     
     public ItemDto get(String code) throws Exception{
-        return null;
+        return itemService.get(code);
     }
     
     public ArrayList<ItemDto> getAll() throws Exception{
-        return null;
+        return itemService.getAll();
     }
 }
